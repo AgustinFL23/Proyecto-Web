a69c385038ec130068b68832a1153b97b70fde5b
AgustinFL23 <127245897+AgustinFL23@users.noreply.github.com>
2024-05-24 11:27:20 -0600

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,8 @@
-inicio
+var elementos = document.querySelectorAll('.Arrastrable');
+function manejarClic(evento) {
+  var idElemento = evento.target.id; 
+  console.log("ID del elemento: " + idElemento); 
+}
+elementos.forEach(function(elemento) {
+  elemento.addEventListener('click', manejarClic);
+});
